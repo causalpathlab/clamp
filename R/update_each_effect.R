3417ac36776a28b7cb90f7a150305eb31bf87b99
Ming Yuan <ymyuan@student.ubc.ca>
2024-11-05 12:11:38 -0800

    Update update_each_effect.R
    specific for causal-susie

--- R/update_each_effect.R
+++ R/update_each_effect.R
@@ -1,35 +1,60 @@
-#' @title Update each effect once in linear model; each sub-model is a weighted SER.
+#' @title Update each effect once in a linear model; each sub-model is a weighted SER.
+#'
 #' @param X An (n by p) matrix of regressor variables
+#'
 #' @param y An n vector of response variable
+#'
 #' @param s A clamp fit
+#'
 #' @param W An (n by p) matrix of weights. If \code{W=NULL}, it reduces to an SER
+#'
+#' @param mle_variance_estimator The estimation method of the variance of the MLEs,
+#' or equivalently, the variance of (Horvitz-Thompson) ATE estimators.
+#' \code{"bootstrap"} estimates the variances from \code{nboots} bootstrap replicates,
+#' \code{"sandwich"} uses sandwich robust variance estimators.
+#' \code{"naive"} calculates the variances from the weighted least square model,
+#' treating weights as constants (not recommended).
+#'
+#' @param nboots The number of bootstrap replicates. By default, \code{nboots=100}.
+#'
+#' @param seed Random seed utilized when \code{mle_variance_estimator="bootstrap"}.
+#' If \code{seed=NULL}, its default is \code{Sys.time()}.
+#'
 #' @param estimate_prior_variance boolean indicating whether to
 #'   estimate prior variance
+#'
 #' @param check_null_threshold Float, a threshold on the log scale to
 #'   compare likelihood between current estimate and zero the null
+#'
 #' @param abnormal_proportion a value between 0 and 1. If the number of detected
 #'   abnormal subjects exceeds \eqn{abnormal_proportion * nrow(X)},
 #'   stop fitting the model.
+#'
 #' @param robust_method A string, whether and which robust method is applied
 #'   when fitting the model. \code{robust_method="none"} specifies that no
 #'   robust method is applied. \code{robust_method="huber"} specifies that the
 #'   Huber weighting method is applied.
+#'
 #' @param robust_estimator A string, which robust estimator is applied.
 #'   \code{robust_estimator="M"} indicates the M-estimator is applied, and
 #'   \code{robust_estimator="S"} indicates the S-estimator is applied.
 #'
 update_each_effect <- function (X, y, s, W=NULL, model,
-                                estimate_prior_variance = FALSE,
-                                estimate_prior_method = "optim",
-                                check_null_threshold = 0,
-                                abnormal_proportion = 0.5,
-                                robust_method = c("none", "huber"),
-                                robust_estimator = c("M", "S")) {
+                  mle_variance_estimator = c("bootstrap", "sandwich", "naive"),
+                  nboots = 100,
+                  seed = NULL,
+                  estimate_prior_variance = FALSE,
+                  estimate_prior_method = "optim",
+                  check_null_threshold = 0,
+                  abnormal_proportion = 0.5,
+                  robust_method = c("none", "huber"),
+                  robust_estimator = c("M", "S")) {
 
   if (!estimate_prior_variance) estimate_prior_method = "none"
 
   robust_method    <- match.arg(robust_method)
   robust_estimator <- match.arg(robust_estimator)
+  mle_variance_estimator <- match.arg(mle_variance_estimator)
 
   # Check weight matrix W
   if (!is.null(W) &
@@ -124,7 +149,11 @@ update_each_effect <- function (X, y, s, W=NULL, model,
                                 prior_inclusion_prob = s$pie,
                                 prior_varB = s$prior_varB[l],
                                 optimize_prior_varB = estimate_prior_method,
-                                check_null_threshold = check_null_threshold)
+                                check_null_threshold = check_null_threshold,
+                                mle_variance_estimator = mle_variance_estimator,
+                                nboots = nboots,
+                                seed = seed)
+
 
       # Update the variational estimate of the posterior distributions.
       s$mu[l,]        = res$mu
