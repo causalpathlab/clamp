f733c99e99e906ea6d1af3cd1aeb72bfe98a2477
Ming Yuan <ymyuan@student.ubc.ca>
2025-01-28 13:53:36 -0800

    Update model_linear.R

--- R/model_linear.R
+++ R/model_linear.R
@@ -6,7 +6,7 @@
 #'
 
 # Expected log-likelihood for a (ordinary) susie fit for
-# a linear regression model
+# a linear regression model as a whole.
 #' @keywords internal
 Eloglik_linear = function (X, Y, s) {
   n = nrow(X)
@@ -16,9 +16,26 @@ Eloglik_linear = function (X, Y, s) {
 
 # Expected squared residuals.
 # s$Xr is column sum of Xr_L
-# For family %in% c("logistic", "poisson"), Y is be the pseudo response.
+# For family %in% c("logistic", "poisson"), there should not be a sigma2.
+
 get_ER2 = function (X, Y, s) {
+
   Xr_L = compute_MXt(s$alpha * s$mu, X) # L by N matrix
   postb2 = s$alpha * s$mu2 # Posterior second moment.
+
   return(sum((Y - s$Xr)^2) - sum(Xr_L^2) + sum(attr(X,"d") * t(postb2)))
+}
+
+
+# @title posterior expected log-likelihood for a single effect regression
+# @param X an n by p matrix of covariates
+# @param Y an n vector of regression outcome
+# @param s2 the residual variance
+# @param Eb the posterior mean of b (p vector) (alpha * mu)
+# @param Eb2 the posterior second moment of b (p vector) (alpha * mu2)
+SER_posterior_e_loglik = function (X, Y, s2, Eb, Eb2) {
+  n = nrow(X)
+  return(-0.5*n*log(2*pi*s2) - 0.5/s2*(sum(Y*Y)
+                                       - 2*sum(Y*compute_Xb(X,Eb))
+                                       + sum(attr(X,"d") * Eb2)))
 }
