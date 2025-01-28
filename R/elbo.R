6b902b1643fe96b7f754462e115dc38f6eb14ad0
Ming Yuan <ymyuan@student.ubc.ca>
2025-01-28 13:53:04 -0800

    Update
    use composite likelihood. different from the same function in susie.

--- R/elbo.R
+++ R/elbo.R
@@ -2,20 +2,24 @@
 # @param data A flash data object.
 # @param f A flash fit object.
 # @keywords internal
-get_elbo = function (X, Y, s, W=NULL, model) {
+
+# get_elbo = function (X, y, s, W=NULL, model) {
+get_elbo = function(s) {
   include_idx <- s$prior_varB > 1e-9
 
   post_varB <- s$mu2 - s$mu^2  # avoid 0
-  revKL <- sapply(1:nrow(s$alpha),
-                  function(l){
-                    reverseKL_l_fun(alpha.l      = s$alpha[l,],
-                                    post_varB.l  = post_varB[l,],
-                                    mu.l         = s$mu[l,],
-                                    pie.l        = s$pie[l],
-                                    prior_varB.l = s$prior_varB[l])})
-  revKL <- revKL[include_idx]
 
-  return(sum(model$loglik(X,Y,s)) + sum(revKL))  ##Eloglik!!
+  revKL <- reverseKL_var_vs_prior(alpha = s$alpha,
+                                  post_varB = post_varB,
+                                  mu = s$mu,
+                                  pie = s$pie,
+                                  prior_varB = s$prior_varB)
+
+  revKL <- revKL[include_idx]
+  Eloglik <- s$Eloglik[include_idx]
+
+  # return(sum(model$loglik(X,Y,s)) + sum(revKL))  ##Eloglik!!
+  return( mean(Eloglik) + sum(revKL) )  ##Eloglik!!
 }
 
 # to avoid log(0)...
@@ -23,31 +27,18 @@ get_elbo = function (X, Y, s, W=NULL, model) {
   log(t+.Machine$double.eps)
 }
 
-# Compute the (layer-wise) reverse KL of the posterior and prior distribution
-# @param alpha.l     an p-dim vector, posterior inclusion probabilities
-# @param post_varB.l an p-dim vector, posterior variance of B
-# @param mu.l         a p-dim vector, poterior mean of B
-# @param pie.l        a number, prior inclusion probabilities
-# @param prior_varB.l a number, prior variance of B
-reverseKL_l_fun <- function(alpha.l, post_varB.l, mu.l,
-                                 pie.l, prior_varB.l) {
-  sum(0.5*alpha.l * (1+.loge(post_varB.l)-.loge(prior_varB.l) -
-                         (mu.l^2+post_varB.l) / prior_varB.l) +
-        alpha.l * (.loge(pie.l) - .loge(alpha.l)))
+## Reverse KL of the variational and prior distributions of all layers
+#' @param alpha     an L by p matrix, posterior inclusion probabilities
+#' @param post_varB an L by p matrix, posterior variance of Beta
+#' @param mu        an L by p matrix, posterior mean of Beta
+#' @param pie       a  p-dim vector, prior inclusion probabilities
+#' @param prior_varB an L-dim vector, prior variance of Beta (one for each layer)
+#'
+reverseKL_var_vs_prior <- function(alpha, post_varB, mu, pie, prior_varB) {
+  rowSums(
+    0.5*alpha*( 1 + sweep(.loge(post_varB), 1, .loge(prior_varB), "-") -
+                  sweep(mu^2 + post_varB, 1, prior_varB, "/")  ) +
+      alpha * sweep(-.loge(alpha), 2, .loge(pie), "+")
+  )
 }
-
-# @title posterior expected log-likelihood for a single effect regression
-# @param X an n by p matrix of covariates
-# @param Y an n vector of regression outcome
-# @param s2 the residual variance
-# @param Eb the posterior mean of b (p vector) (alpha * mu)
-# @param Eb2 the posterior second moment of b (p vector) (alpha * mu2)
-SER_posterior_e_loglik = function (X, Y, s2, Eb, Eb2) {
-  n = nrow(X)
-  return(-0.5*n*log(2*pi*s2) - 0.5/s2*(sum(Y*Y)
-                                       - 2*sum(Y*compute_Xb(X,Eb))
-                                       + sum(attr(X,"d") * Eb2)))
-}
-
-
 
---
+++ R/model_weighted_linear.R
@@ -0,0 +1,59 @@
+#' @title Functions related to weighted single effect regression model
+#'
+#' @description This file defines functions related to weighted linear
+#' regression model
+#'
+
+# Expected (composite) log-likelihood of layer-l weighted single effect regression.
+#'
+#' @param X n by p matrix, centralized by
+#'        \code{sapply(as.matrix(1:ncol(X)),
+#'              function(j) { X[,j] - weighted.mean(x=X[,j], w=W[,j])}) }
+#' @param y n by p matrix, centralized by
+#'        \code{sapply(as.matrix(1:ncol(X)),
+#'        function(j) { y - weighted.mean(x=y, w=W[,j])}) }
+#' @param W n by p matrix of weights
+#' @param s a clamp fit, containing a list of posterior distributions
+#' @param alpha a p-vector of posterior inclusion probabilities
+#' @param mu a p-vector of posterior means (first moments)
+#' @param mu2 a p-vector of posterior second moments
+#'
+#' @keywords internal
+WSER_posterior_e_loglik = function (X, y, W, alpha, mu, mu2,
+                         residual_variance) {
+  n = nrow(X)
+
+  EWR2 <- get_EWR2_l(X, y, W, alpha, mu, mu2)
+
+  return(-n/2*log(2*pi*residual_variance) + 1/2*sum(alpha*colSums(log(W))) -
+           1/(2*residual_variance) * EWR2)
+}
+
+#' Expectation of weighted sum of squared residual (WRSS) of layer l
+#'
+#'  X : n by p matrix, centralized by
+#'        \code{sapply(as.matrix(1:ncol(X)),
+#'              function(j) { X[,j] - weighted.mean(x=X[,j], w=W[,j])}) }
+#'  y: n by p matrix, centralized by
+#'        \code{sapply(as.matrix(1:ncol(X)),
+#'        function(j) { y - weighted.mean(x=y, w=W[,j])}) }
+#'  W: n by p matrix of weights
+#'  alpha: a p-vector of posterior inclusion probabilities
+#'  mu: a p-vector of posterior means (first moments)
+#'  mu2: a p-vector of posterior second moments
+#'
+get_EWR2_l <- function(X, y, W, # s=NULL,
+                     alpha=NULL, mu=NULL, mu2=NULL) {
+
+  if (is.null(alpha) | is.null(mu) | is.null(mu2)) {
+    stop("Specify `alpha, mu, mu2`!")
+  }
+
+  postb  <- alpha * mu
+  postb2 <- alpha * mu2
+
+  EWR2 <- sum(sweep(W*(y^2), 2, alpha, "*")) -
+    2 * sum(sweep(W*X*y, 2, postb, "*")) + sum(sweep(W*(X^2), 2, postb2, "*"))
+
+  return(EWR2)
+}
