ccd6c795abbe708439fd41257d650b3842b91ddb
Ming Yuan <ymyuan@student.ubc.ca>
2025-02-25 11:11:58 -0800

    Update elbo.R

--- R/elbo.R
+++ R/elbo.R
@@ -5,21 +5,19 @@
 
 # get_elbo = function (X, y, s, W=NULL, model) {
 get_elbo = function(s) {
-  include_idx <- s$prior_varB > 1e-9
 
   post_varB <- s$mu2 - s$mu^2  # avoid 0
 
-  revKL <- reverseKL_var_vs_prior(alpha = s$alpha,
-                                  post_varB = post_varB,
-                                  mu = s$mu,
-                                  pie = s$pie,
-                                  prior_varB = s$prior_varB)
+  revKL <- reverseKL_va_vs_prior(alpha = s$alpha,
+                                 post_varB = post_varB,
+                                 mu = s$mu,
+                                 pie = s$pie,
+                                 prior_varB = s$prior_varB)
 
-  revKL <- revKL[include_idx]
-  Eloglik <- s$Eloglik[include_idx]
+  Eloglik <- s$Eloglik
 
-  # return(sum(model$loglik(X,Y,s)) + sum(revKL))  ##Eloglik!!
-  return( mean(Eloglik) + sum(revKL) )  ##Eloglik!!
+  # return(sum(model$loglik(X,Y,s)) + sum(revKL))
+  return( mean(Eloglik) + sum(revKL) )
 }
 
 # to avoid log(0)...
@@ -34,11 +32,15 @@ get_elbo = function(s) {
 #' @param pie       a  p-dim vector, prior inclusion probabilities
 #' @param prior_varB an L-dim vector, prior variance of Beta (one for each layer)
 #'
-reverseKL_var_vs_prior <- function(alpha, post_varB, mu, pie, prior_varB) {
+reverseKL_va_vs_prior <- function(alpha, post_varB, mu, pie, prior_varB) {
+
   rowSums(
     0.5*alpha*( 1 + sweep(.loge(post_varB), 1, .loge(prior_varB), "-") -
-                  sweep(mu^2 + post_varB, 1, prior_varB, "/")  ) +
+                  sweep(mu^2+post_varB, 1, prior_varB+.Machine$double.eps, "/")  ) +
       alpha * sweep(-.loge(alpha), 2, .loge(pie), "+")
   )
 }
+
+## If variances of variational (posterior) and prior distribution are both 0,
+## then this function will return a certain fixed value (0.5).
 
